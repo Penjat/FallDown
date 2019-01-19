2e313041061618abbab6d9dc19cbe3014735e56d
Penjat <theSpencerSymington@Gmail.com>
2019-01-19 12:49:58 -0800

    Initial Commit

---
+++ FallDown/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  FallDown
+//
+//  Created by Spencer Symington on 2019-01-19.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ FallDown/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  FallDown
+//
+//  Created by Spencer Symington on 2019-01-19.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
